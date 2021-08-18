669d8bf8f6905cfe05f69874c24259be9682fa31
Marty Zhang <marty8zhang@gmail.com>
2021-08-18 14:26:48 +1000

    Add comments for `useContext()`.

--- src/components/ContextHook.tsx
+++ src/components/ContextHook.tsx
@@ -2,6 +2,11 @@ import React, { useContext } from 'react';
 import { ContextHookContext } from '../pages/Hooks';
 
 export default function ContextHook() {
+  /*
+   * `useContext(SomeContext)` is yet another alternative to use context in a class component or
+   * `<SomeContext.Consumer />`. The way of creating a context or using `<SomeContext.Provider />`
+   * remains the same.
+   */
   const contextValue = useContext(ContextHookContext);
 
   return (
